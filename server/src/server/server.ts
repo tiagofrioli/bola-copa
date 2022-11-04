2632df954487ec288e4a4231362d5a140e359e55
C1322591 Tiago Frioli Leonel <c1322591@interno.bb.com.br>
2022-11-04 00:41:58 -0300

    first commit

---
+++ server/src/server/server.ts
@@ -0,0 +1,26 @@
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+async function start() {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+    return { count: 0 };
+  });
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
+}
+
+start();
